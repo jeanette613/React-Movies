48749d2a2fbd2332ca77cbc873fff531d1bd3544
Melina C <melange@Melinas-MacBook-Air.local>
2022-04-02 23:46:53 -0400

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const apiKey = '98e3fb1f';
+
+    const [movie, setMovie] = useState(null);
+
+    const getMovie = async (searchTerm) => {
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`);
+        const data = await response.json();
+        setMovie(data);
+    }
+
+
+    return (
+        <div className="App">
+            <Form moviesearch={getMovie} />
+            <MovieDisplay movie={movie} />
+        </div>
+    )
 }
-
-export default App;
